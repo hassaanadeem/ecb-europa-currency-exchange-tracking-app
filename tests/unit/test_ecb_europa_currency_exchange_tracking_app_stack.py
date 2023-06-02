7039817e1570968925c82b84664b380191dd933a
Hassan Nadeem <hassan.nadeem@gsquad.io>
2023-06-02 17:16:35 +0500

    added tests for stack creation

--- tests/unit/test_ecb_europa_currency_exchange_tracking_app_stack.py
+++ tests/unit/test_ecb_europa_currency_exchange_tracking_app_stack.py
@@ -1,21 +1,70 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
-from ecb_europa_currency_exchange_tracking_app.ecb_europa_currency_exchange_tracking_app_stack import EcbEuropaCurrencyExchangeTrackingAppStack
+from ecb_europa_currency_exchange_tracking_app.ecb_europa_currency_exchange_tracking_app_stack import (
+    EcbEuropaCurrencyExchangeTrackingAppStack,
+)
 
 
-def test_sqs_queue_created():
+def test_dynamodb_table_created():
     app = core.App()
-    stack = EcbEuropaCurrencyExchangeTrackingAppStack(app, "ecb-europa-currency-exchange-tracking-app")
+    stack = EcbEuropaCurrencyExchangeTrackingAppStack(
+        app, "ecb-europa-currency-exchange-tracking-app"
+    )
     template = assertions.Template.from_stack(stack)
+    template.has_resource_properties(
+        "AWS::DynamoDB::Table",
+        {
+            "KeySchema": [{"AttributeName": "id", "KeyType": "HASH"}],
+            "AttributeDefinitions": [{"AttributeName": "id", "AttributeType": "S"}],
+        },
+    )
+    template.has_resource("AWS::DynamoDB::Table", {"DeletionPolicy": "Delete"})
 
-    template.has_resource_properties("AWS::SQS::Queue", {
-        "VisibilityTimeout": 300
-    })
 
-
-def test_sns_topic_created():
+def test_etl_routine_lambda_created():
     app = core.App()
-    stack = EcbEuropaCurrencyExchangeTrackingAppStack(app, "ecb-europa-currency-exchange-tracking-app")
+    stack = EcbEuropaCurrencyExchangeTrackingAppStack(
+        app, "ecb-europa-currency-exchange-tracking-app"
+    )
     template = assertions.Template.from_stack(stack)
+    template.has_resource_properties(
+        "AWS::Lambda::Function",
+        {
+            "Handler": "etl_routine.handler",
+            "Runtime": "python3.9",
+            "Timeout": 240,
+            "Environment": {"Variables": {"TABLE_NAME": {}}},
+        },
+    )
 
-    template.resource_count_is("AWS::SNS::Topic", 1)
+
+def test_rest_endpoint_lambda_created():
+    app = core.App()
+    stack = EcbEuropaCurrencyExchangeTrackingAppStack(
+        app, "ecb-europa-currency-exchange-tracking-app"
+    )
+    template = assertions.Template.from_stack(stack)
+    template.has_resource_properties(
+        "AWS::Lambda::Function",
+        {
+            "Handler": "rest_endpoint.handler",
+            "Runtime": "python3.9",
+            "Timeout": 60,
+            "Environment": {"Variables": {"TABLE_NAME": {}}},
+        },
+    )
+
+
+def test_rest_api_gateway_created():
+    app = core.App()
+    stack = EcbEuropaCurrencyExchangeTrackingAppStack(
+        app, "ecb-europa-currency-exchange-tracking-app"
+    )
+    template = assertions.Template.from_stack(stack)
+    template.has_resource_properties(
+        "AWS::ApiGateway::RestApi", {"Name": "rest-api-rates"}
+    )
+    template.has_resource_properties(
+        "AWS::ApiGateway::Resource", {"PathPart": "exchange-rate-info"}
+    )
+    template.has_resource_properties("AWS::ApiGateway::Method", {"HttpMethod": "GET"})
